87a5866f7260bc6bfe5175fa7e5bd1ec7d61f3f3
Josef Vybíral <josef.vybiral@gmail.com>
2015-03-09 17:06:37 +0100

    Sample environment configuration file.

---
+++ env.sample.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Sample configuration
+ * 
+ * Script is set to look for .env.php for production and .env.local.php for local development
+ */
+
+return array(
+  'ENCRYPTION_KEY' => 'YourLaravelGeneratedEncryptionKey',
+
+  'DB_HOST'        => 'DBHOST',
+  'DB_NAME'        => 'DBNAME',
+  'DB_USER'        => 'DBUSER',
+  'DB_PASS'        => 'DBPASS',
+  'DB_PREFIX'      => ''
+);
